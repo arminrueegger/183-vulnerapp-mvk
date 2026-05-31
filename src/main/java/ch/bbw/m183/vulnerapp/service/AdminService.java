0649ea46c04d11aee6051281cf00ca82062227fb
arminr <armin.rueegger@ergon.ch>
2026-05-31 09:18:00 +0200

    hash user passwords, set roles and change admin service

--- src/main/java/ch/bbw/m183/vulnerapp/service/AdminService.java
+++ src/main/java/ch/bbw/m183/vulnerapp/service/AdminService.java
@@ -9,6 +9,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +20,10 @@ public class AdminService {
 
 	private final UserRepository userRepository;
 
+	private final PasswordEncoder passwordEncoder;
+
 	public UserEntity createUser(UserEntity newUser) {
+		newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
 		return userRepository.save(newUser);
 	}
 
@@ -33,8 +37,12 @@ public class AdminService {
 
 	@EventListener(ContextRefreshedEvent.class)
 	public void loadTestUsers() {
-		Stream.of(new UserEntity().setUsername("admin").setFullname("Super Admin").setPassword("super5ecret"),
-						new UserEntity().setUsername("fuu").setFullname("Johanna Doe").setPassword("bar"))
-				.forEach(this::createUser);
+		if (userRepository.count() > 0) {
+			return;
+		}
+		Stream.of(
+				new UserEntity().setUsername("admin").setFullname("Super Admin").setPassword("super5ecret").setRole("ADMIN"),
+				new UserEntity().setUsername("fuu").setFullname("Johanna Doe").setPassword("barbarbar1").setRole("USER")
+		).forEach(this::createUser);
 	}
 }
